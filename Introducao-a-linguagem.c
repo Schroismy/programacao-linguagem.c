f8425404f634238a69f14b2da3368e91fa307f8b
César Schroeder <99970586+Schroismy@users.noreply.github.com>
2022-04-01 19:44:16 -0300

    Create Introducao-a-linguagem.c

---
+++ Introducao-a-linguagem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  // Este é um comentário em (uma unica linha)
+  // Só limita o comentáro para que está na direita.
+  // Este é outro comentário
+  
+  /*
+  Este é um comentário em (multiplas linhas)
+  Linha 1
+  Linha 2
+  Linha 3
+  Linha 4
+  */
+  
+  printf("Hello Word!\n");
+ 
+  system("pause");
+  return 0;
+}
