e3a6618c13982295c3fba0acf467867337e10bac
César Schroeder <99970586+Schroismy@users.noreply.github.com>
2022-04-01 20:19:19 -0300

    Create Condicao.c

---
+++ Condicao.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+  /*
+  CONDIÇÃO
+-Condições estão presentes em cada instanteda nossa vida.
+Ex: Se eu for na padaria então copmprarei pão.
+Ex: Se eu estou com sede então tomarei água.
+Ex: Se o carro quebrar então chamarrei o mecânico.
+
+-Seus programas nem sempre serão sequencias simples de comamandos.
+-Nem todas as linhas de códogo serão executadas.
+-Uma condição pode ter dois valores:
+-->Verdadeiro(True)
+-->Falso (False)
+
+Exemplos de condição
+-idade > 30(Idade é  maior que 30?)
+-media >= 7 (Média é maior ou igual a 7?)
+-alunosAprovados < 10 (A Quantidade de alunos aprovados é menor qo que 10?)
+
+Estrutura de Seleção na Linguagem  C.
+-Estrutura de  seleção (decisão ou condicionais) possibilitam criar desvios na execução de um algoritmo.
+-Percorrer um caminho diferente de execução.
+-Os devios ocorrem atráves de testes lógicos (V ou F)
+
+Principais estruturas de decisão incluem:
+-if(teste lógico) {...}
+-if(teste lógico) {...} else {...}
+-if(teste lógico) {...} else if {teste lógico} ... else{...}
+-switch(variável) {...} 
+  */
+
+
