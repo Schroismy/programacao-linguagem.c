fdadcac981ada24a045494822138ab442278fe56
César Schroeder <99970586+Schroismy@users.noreply.github.com>
2022-04-01 19:58:03 -0300

    Create Operadores.c

---
+++ Operadores.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+
+int main(void) {
+/*
+ Operador é um simbolo que indica ao copilador para efetuar uma manipulação específica matemática ou lógica 
+  
+  OPERADORES ARITMÉTICOS: São simbolos par utilizados para realizar as operaçãoes aritméticas elementares
+
+  +Adição
+  -Subtração
+  *Multiplicação
+  /Divisão
+ */
+
+/*   float n1, n2, result;
+  printf("Informe o primeiro valor: ");
+  scanf("%f", &n1);
+
+  printf("Informe o segundo valor: ");
+  scanf("%f", &n2);
+  
+   result = n1 / n2;
+   printf("resultado: %.2f\n", result);
+
+*/
+  
+/*
+  RESTO DA DIVISÃO (MÓDULO)
+-Representado por %
+-A divisão inteira
+
+-Quando realizamos uma divisão de duas variáveis inteiras,a parte fracionada é eliminada de forma automática, ou então é "truncada".
+
+-Como a parte  fracionada é descartada, temos que o resultado de um divisão inteira é sempre o número inteiro.
+
+Exemplo:
+7/2 tem como resultado 3 e não 3,5 que é um número real
+2/3 é zero e não 0,66.
+
+-O operador % retorna o resto da divisão de um número por outro.
+5 % 2 = 1
+5-2=3-2=1
+4 % 2 = 0
+4-2=2-2=0
+7 % 3 = 1
+7-3=4-3=1
+-Em alguns casos, este operador é bastante útil.
+-Por exemplo, em algumas aplicaçãoes é necessario extrair dígitos de um valor inteiro.
+-Existem outras situações, que serão abordadas quando estudarmos as Estruturas de Decisão.
+
+*/
+//int n1, n2, result;
+//printf("Informe o primeiro valor: ");
+//scanf("%d", &n1);
+
+// printf("Informe o segundo valor: ");
+//scanf("%d", &n2);
+  
+//result = n1 % n2;
+   //printf("Resultado: %d\n", result);
+
+  
+
+/*
+int n1, n2, result, result2;
+printf("Informe o primeiro valor: ");
+scanf("%d", &n1);
+
+result = n1 % 10; // resto da divisão
+result2 = n1 / 10;// divisão por 10 
+printf("Resultado: %d\n", result);
+printf("Resultado: %d\n", result2);
+
+
+*/
+
+ 
+  
+/*
+  Operadores de Auto-incremento e auto- decremento 
+++ Auto-incremento ==> Aumenta uma unidade no valor da variável 
+-Utilizar x++ equivalente a fazer x = x + 1; ou seja, soma uma unidade ao valor de x.
+
+-- Auto-decremento ==> Diminui uma unidade no valor da variável
+-Utilizar x-- equivale a  fazer x = x - 1; ou seja, subtrai um unidade ao valor de x.
+*/
+
+  //int numero;
+  //printf("Informe o primeiro valor: ");
+  //scanf("%d", &numero);
+  
+
+  // numero++;
+   //printf("Resultado: %d\n", numero);
+   //int numero;
+  //printf("Informe o primeiro valor: ");
+  //scanf("%d", &numero);
+  
+
+   //numero = numero + 1; // equivalente a numero++
+  //printf("Resultado: %d\n", numero);
+  
+
+ //int numero;
+  //printf("Informe o primeiro valor: ");
+  //scanf("%d", &numero);
+  
+
+   //numero--;
+   //printf("Resultado: %d\n", numero);
+  
+  
+
+/*
+Operadores Compostos de atribuição
+-Os operadores compostos de atribuição são: +=, -=, *=, /=, são muito utilizados em expressões matemáticas como atalhos para as operações aritméticas mais comuns.
+
+Exemplo: ao invés de excrever: num = num + 5;
+numero +=10;
+pode-se utilizar a seguinte expressão: num + =5;
+
+Outros exemplos de utilização:
+num -= 1;//equivale a num = num - 1
+num *= 2;//equivale a num = num * 2
+num /= 2;//equivale a num = num / 2
+
+
+*/
+
+/*
+
+*/
+
+}
