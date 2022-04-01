ddc0b5b63fce0c2a9ad7b2c6d3ac7e7907fe9fda
César Schroeder <99970586+Schroismy@users.noreply.github.com>
2022-04-01 19:37:10 -0300

    Contagem-regressiva-e-aumentativa
    Aqui eu passei o meu código do Replit para o Code Block.
    Nó inicio foi um pouco difícil mas depois eu peguei o jeito.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main (void)
+        {
+
+        int cont;
+        //Contagem Regressiva
+        for(cont = 10; cont > 0; cont--) {
+            printf("\n%d", cont );
+        }
+        return 0;
+        }
